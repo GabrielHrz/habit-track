9ed0a6167d17cf8ac810a2cc1dff9cece559e159
Gabriel Hernandez <gabrielalexanderhr@gmail.com>
2026-06-17 01:44:32 +0100

    tailwindcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
-<h1>Welcome to home page</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<p>Ola {{ $name }}</p>
-<p>Tus habitos son:</p>
-<ul>
-    @foreach ($habits as $habit)
-        <li>{{ $habit }}</li>
-    @endforeach
-</ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>{{ config('app.name') }}</title>
+</head>
+
+<body>
+    <h1 class="text-2xl font-bold text-blue-500">Welcome to the Home Page</h1>
+</body>
+
+</html>

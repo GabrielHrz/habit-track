28613cfd4cc8822043d13430173e2336feb81a07
Gabriel Hernandez <gabrielalexanderhr@gmail.com>
2026-06-21 01:12:17 +0100

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-
 Route::get('/', [SiteController::class, 'index']);
+Route::get('/login',[LoginController::class, 'index']);
